f95254e60f3bc8c4770e21bb32ea9b9e9e8ad8ea
mohgad <72159576+mohgad@users.noreply.github.com>
2022-03-08 14:58:23 +0200

    Update __manifest__.py

--- construction_delivery/__manifest__.py
+++ construction_delivery/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Donstruction Delivery",
+    'name': "Construction Task Delivery",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
